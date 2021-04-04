aba4e729ab5f59160d60b2848ca1862186146af3
mit <mit945.mc@gmail.com>
2021-04-03 22:48:18 -0700

    cool

--- controller/route.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "net/http"
-
-func Register() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", ping())
-	return mux
-}
--- server.go
+++ server.go
@@ -2,14 +2,24 @@ package main
 
 import (
     "net/http"
-    "github.com/TheOnly-Co/to_do/controller"
+    "github.com/TheOnly-Co/to_do/views"
+    "encoding/json"
 )
 
 
 
 func main() {
-    mux := controller.Register()
-    
+    mux := http.NewServeMux()
+    mux.HandleFunc("/ping",func(w http.ResponseWriter, r *http.Request){
+        if r.Method == http.MethodGet {
+        	data := views.Response{
+        		Code: http.StatusOK,
+        		Body: "pong",
+        	}
+        	json.NewEncoder(w).Encode(data)
+        	
+        }
+	})
    
 	http.ListenAndServe("localhost:3000", mux)
 }
